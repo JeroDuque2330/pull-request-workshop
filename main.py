91c5193b11734c79f372c9a9cfa725d7df989a90
JeroDuque2330 <jeroduque2007@gmail.com>
2024-08-28 07:19:17 -0500

    "cambios"

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # El siguiente programa recibe una entrada (input) del usuario y retorna el número de dígitos y de letras que contenga el mismo
 
 # Itendifica los errores y modifica el programa para que funcione según el enunciado
-user_input = "Dan1el"
+user_input = input ("NGRESA UN USUARIO CON NUMEROS Y LETRAS")
 
 digits = 0
 letters = 0
 
 for i in user_input:
     if i.isdigit():
-        digits = 0
+        digits += 1
     elif i.isalpha():
-        letters = 0
+        letters += 1
 
 print(" The input string", user_input, "has", letters, "letters and", digits, "digits.")
